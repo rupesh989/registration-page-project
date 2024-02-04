eac2f8d1c95772fa61a2f1b53dbbff1a6cdfffee
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-04 19:05:47 +0530

    updated

---
+++ src/db/conn.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb://localhost:27017/projectRegistration",
+
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      UseCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log(`connection successful`);
+  })
+  .catch((e) => {
+    console.log(`No Connection`);
+    console.log(e);
+  });
