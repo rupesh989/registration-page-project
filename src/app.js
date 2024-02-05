f512e53b5dbd9d66880d2cdad238d1dd354a63fa
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-05 22:56:29 +0530

    Added public folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
+const path = require("path");
 const app = express();
 
 require("./db/conn");
 
 const port = process.env.PORT || 3000;
 
+// const static_path = path.join(__dirname);
+// app.use(express.static());
+console.log(path.join(__dirname, "../public"));
 app.get("/", (req, res) => {
   res.send("Hello from Rupesh Bavaskar");
 });
