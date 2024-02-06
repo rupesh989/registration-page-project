78a3a33b2fa8e6c8300dfc9c4766f74f22b189f9
rupesh989 <rupeshbavaskar24@gmail.com>
2024-02-06 22:42:34 +0530

    Added partials

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const path = require("path");
 const app = express();
+const hbs = require("hbs");
 
 require("./db/conn");
 
 const port = process.env.PORT || 3000;
 
-// const static_path = path.join(__dirname, "../public");
-// app.use(express.static(static_path)); // delete it after public index deleted
 // console.log(path.join(__dirname, "../public"));
+// const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
+// app.use(express.static(static_path)); // delete it after public index deleted
 app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
 
 app.get("/", (req, res) => {
   // res.send("Hello from Rupesh Bavaskar");
